7bddd82cbd5fa90768a3d5430bc6bdd545d92741
bmlsj <97381846+bmlsj@users.noreply.github.com>
2022-09-26 10:25:52 +0900

    [level 1] Title: 시저 암호, Time: 1.42 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    answer = ''
+    for c in s:
+        cInt = ord(c)
+        if c != " ":
+            if cInt <= 90:
+                cInt = (cInt - 65 + n) % 26
+                answer += chr(cInt+65)
+            else:
+                cInt = (cInt - 97 + n) % 26
+                answer += chr(cInt+97)
+        else:
+            answer += " "
+    return answer
