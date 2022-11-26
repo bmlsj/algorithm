c4aab4a762adc89a8cb40be040c89e4b8d622d8d
bmlsj <97381846+bmlsj@users.noreply.github.com>
2022-11-26 22:34:54 +0900

    [Bronze III] Title: 이진수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3460. 이진수/이진수.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for i in range(t):
+    ans = []
+    n = int(input())
+
+    while n >= 1:
+        ans.append(n % 2)
+        n = n // 2
+
+    for j in range(len(ans)):
+        if ans[j] == 1:
+            print(j, end=" ")
+
+    print("")
