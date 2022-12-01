a59f7f5157e0462223b37237e3661f7d3b25c6ea
bmlsj <97381846+bmlsj@users.noreply.github.com>
2022-12-01 20:43:36 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 224 ms, Memory: 118648 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,35 +1,31 @@
+import sys
 
-# 연산자는 앞에서부터
-# 나눗셈은 정수 나눗셈으로 몫만 취함
-# 음수를 양수로 나눌 때, 양수로 바꾼뒤 몫을 구하고 음수로 변환
-from itertools import permutations
-
+input = sys.stdin.readline
 N = int(input())
 num = list(map(int, input().split()))
-sign_list = list(map(int, input().split()))
-sign = ['+', '-', '*', '/']
+op = list(map(int, input().split()))  # +, -, *, //
 
-op = []
-
-for i in range(len(sign_list)):
-    for j in range(sign_list[i]):
-        op.append(sign[i])
+maximum = -1e9
+minimum = 1e9
 
 
-result = []
-for i in permutations(op, N-1):
-    calc = num[0]
-    for j in range(1, N):
-        if i[j-1] == '+':
-            calc += num[j]
-        elif i[j-1] == '*':
-            calc *= num[j]
-        elif i[j-1] == '/':
-            calc = int(calc/num[j])
-        elif i[j-1] == '-':
-            calc -= num[j]
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
 
-    result.append(calc)
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
 
-print(max(result))
-print(min(result))
+
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
