237e66b9a20e339f3506e1f77154b949fc5c4672
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-01-13 15:29:00 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.51 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -4,11 +4,8 @@ def solution(numer1, denom1, numer2, denom2):
     r = denom1 * denom2  # 분모
     
     gcd = []
-    j = f
-    if f >= r:
-        j = r
     
-    for i in range(1, f+1):
+    for i in range(1, r+1):
         if f % i == 0 and r % i == 0:
             gcd.append(i)
     
