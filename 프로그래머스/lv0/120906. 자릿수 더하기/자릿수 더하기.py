32712b5994fa998bf07aa56a53187c87c8f79e9f
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-01-16 17:05:55 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,11 @@
 def solution(n):
-
+    ans = 0
+    
+    # 1
+    # while n > 0:
+    #    ans += (n%10)
+    #    n = (n//10)
+        
     return sum(int(i) for i in str(n))
         
         
