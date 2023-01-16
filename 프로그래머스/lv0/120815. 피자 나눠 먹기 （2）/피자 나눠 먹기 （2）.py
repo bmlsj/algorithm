70a980e986c57912dc844c10877936fd10924c40
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-01-16 17:14:31 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -2,9 +2,9 @@ def solution(n):
     
     i = 1
     while True:
-        if (n * i) % 6 == 0:
+        if (6 * i) % n == 0:
             break        
         i += 1
     
-    return (n*i) // 6
+    return i
         
