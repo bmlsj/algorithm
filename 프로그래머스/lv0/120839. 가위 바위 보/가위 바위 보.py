76b4c2b516c7065933182042cbab86734f7c0a6c
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-01-19 05:35:46 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,9 @@
+def solution(rsp):
+    # 가위 2, 바위 0, 보 5
+    
+    game = {'2':'0', '0':'5', '5':'2'}
+    s = ''
+    for i in rsp:
+        if i in game.keys():
+            s += game[i]
+    return s
