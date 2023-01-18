69fab355bb45bd48b21db021c4e8780d2ff95e06
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-01-19 05:25:05 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,4 +1,5 @@
 def solution(n, numlist):
     
-    return [i for i in numlist if i%n == 0]
+    # return [i for i in numlist if i%n == 0]
+    return list(filter(lambda v: v%n==0, numlist))
         
