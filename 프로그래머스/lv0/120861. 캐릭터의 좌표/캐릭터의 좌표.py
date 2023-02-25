3ede0b4901fbe7fba5de6290bf65843615e299a2
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-02-25 19:01:44 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,19 +1,18 @@
 def solution(keyinput, board):
-    col = board[0]  # 7
-    row = board[1]  # 9
+
     start = [0, 0]
-    
+
     for i in keyinput:
-        if i == "left" and start[0] - 1 >= -(col // 2):
-            start[0] -= 1
-            
-        elif i == "right" and start[0] + 1 <= (col // 2):
-            start[0] += 1
-            
-        elif i == "up" and start[1] + 1 <= (row // 2):
+        if i == "up" and (start[1] + 1 <= (board[1] // 2)):
             start[1] += 1
-            
-        elif i == "down" and start[1] - 1 >= -(row // 2):
+
+        elif i == "down" and (start[1] - 1 >= -(board[1] // 2)):
             start[1] -= 1
-            
+
+        elif i == "left" and (start[0] - 1 >= -(board[0] // 2)):
+            start[0] -= 1
+
+        elif i == "right" and (start[0] + 1 <= (board[0] // 2)):
+            start[0] += 1
+
     return start
