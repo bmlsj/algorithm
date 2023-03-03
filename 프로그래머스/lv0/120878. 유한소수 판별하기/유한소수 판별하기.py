ecae143f7e106e0828869422a3b7c07a04e55746
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-03-04 00:29:15 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,5 +1,24 @@
 def solution(a, b):
     
+    num = 1
+    if a % b == 0:
+        return 1
+      
+    
+    for i in range(2, b+1):
+        if a % i == 0 and b % i == 0:
+            num = i
+    
+    b /= num
+    while b % 2 == 0:
+        b /= 2
+    while b % 5 == 0:
+        b /= 5
+    
+    print(b)
+    return 1 if b == 1 else 2
+
+"""    
     g = 1
     for i in range(1, b+1):
         if (a % i == 0) and (b % i == 0):
@@ -12,3 +31,5 @@ def solution(a, b):
         b /= 5
     
     return 1 if b == 1 else 2
+
+"""
