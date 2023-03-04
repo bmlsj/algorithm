23b7d53fcf4f273a10d1f4a53221bd06beb9027b
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-03-04 22:22:28 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.22 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -4,6 +4,6 @@ def solution(sides):
     short = min(sides)
             
     # 주의 : sides의 원소의 길이는 있지만, 다른 한 변의 길이 범위는 존재하지 않음
-    # return len([i for i in range(1, 1001) if sum(sides) > i > max(sides) - min(sides)])
+    return len([i for i in range(1, 2001) if sum(sides) > i > max(sides) - min(sides)])
     
-    return 2*short - 1
+    # return 2 * short - 1
