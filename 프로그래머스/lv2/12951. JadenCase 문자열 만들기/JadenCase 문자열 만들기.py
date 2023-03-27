5fe5aea782384cc5b7da4b3346ca58a3bbdb859b
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-03-27 17:08:46 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -6,12 +6,8 @@ def solution(s):
     for c in sen:
         if c == "":
             ans += ""
-        elif c[0].islower():
-            ans += (c[0].upper())             
-            c = c.lower()
-            ans += c[1:]
         else:
-            ans += c[0]
+            ans += (c[0].upper()) 
             c = c.lower()
             ans += c[1:]
         ans += " "
