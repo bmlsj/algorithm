93127991404dca88afa8ad0df3f139f8aeb28520
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-03-30 22:45:32 +0900

    [Silver IV] Title: 제로, Time: 80 ms, Memory: 32036 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        arr.pop()
+    else:
+        arr.append(num)
+
+print(sum(arr))
