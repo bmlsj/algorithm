d878b6fd0032714ef36f05b20ad48f9002ec623d
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-04-01 09:22:35 +0900

    [Silver III] Title: 1로 만들기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,17 @@
-import sys
-input = sys.stdin.readline
-
 x = int(input())
+dp = {1: 0}  # 1이 되는데 필요한 횟수는 0
 
-d = [0] * (x + 1)
+def rec(n):
+    if n in dp.keys():
+        return dp[n]
+    if (n % 3 == 0) and (n % 2 == 0):
+        dp[n] = min(rec(n // 3) + 1, rec(n // 2) + 1)
+    elif n % 3 == 0:
+        dp[n] = min(rec(n // 3) + 1, rec(n - 1) + 1)
+    elif n % 2 == 0:
+        dp[n] = min(rec(n // 2) + 1, rec(n - 1) + 1)
+    else:
+        dp[n] = rec(n - 1) + 1
+    return dp[n]
 
-for i in range(2, x+1):
-    d[i] = d[i-1] + 1
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i//2] + 1)
-
-    if i % 3 == 0:
-        d[i] = min(d[i], d[i//3] + 1)
-
-print(d[x])
+print(rec(x))
