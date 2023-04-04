f7958d61fca3c6c224c936a62f37b7f1624b9a26
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-04-04 22:05:44 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [0 for i in range(n + 1)]
+arr = [list(map(int, input().split())) for _ in range(n + 1)]
+
+for i in range(n - 1, -1, -1):
+    # i 일에 상담을 하는 것이 퇴사일을 넘기면 상담을 하지 않는다.
+    if i + arr[i][0] > n:
+        dp[i] = dp[i + 1]
+    else:
+        # i 일에 상담을 하는 것과 안하는 것 중 큰 것을 선택
+        dp[i] = max(dp[i + 1], arr[i][1] + dp[i + arr[i][0]])
+
+print(dp[0])
