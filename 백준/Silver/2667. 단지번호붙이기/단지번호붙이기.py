a165de293ffe0844e07668439603b1b846b5e541
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-04-07 11:33:17 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 31316 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,47 +1,35 @@
-
-# 1 : 집이 있는 곳, 0 : 집이 없는 곳
-# 단지별 각 집의 수를 출력
-from collections import deque
-
 n = int(input())
 arr = [list(map(int, input())) for _ in range(n)]
+visited = [[False for _ in range(n + 1)] for _ in range(n + 1)]
 
-def bfs(x, y):
 
-    q = deque()
-    q.append((x, y))
+def dfs(x, y):
 
-    arr[x][y] = 0
-    cnt = 1
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+    if arr[x][y] == 1:
+        global cnt
+        arr[x][y] = 0
+        cnt += 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        dfs(x, y - 1)
+        return True
+    return False
 
-    while q:
-        x, y = q.popleft()
-
-        dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-
-            if arr[nx][ny] == 1:
-                arr[nx][ny] = 0
-                q.append((nx, ny))
-                cnt += 1
-
-    return cnt
 
 k = 0
+cnt = 0
 cnt_list = []
 for i in range(n):
-    cnt = 0
     for j in range(n):
-        if arr[i][j]:
+        if dfs(i, j):
+            cnt_list.append(cnt)
             k += 1
-            cnt_list.append(bfs(i, j))
+            cnt = 0
 
-cnt_list.sort()
 print(k)
+cnt_list.sort()
 for i in cnt_list:
     print(i)
