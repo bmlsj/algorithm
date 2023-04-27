3a6b48c8b9242e646e8b6a9ed77cadb5b2ddfd27
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-04-27 21:20:03 +0900

    [Silver III] Title: N과 M (3), Time: 2864 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,6 +1,5 @@
 n, m = map(int, input().split())
 arr = [0] * (m + 1)
-visited = [False] * (n + 1)
 
 def func(k):
     if k == m:
@@ -10,10 +9,7 @@ def func(k):
         return
 
     for i in range(1, n + 1):
-        if not visited[i]:
             arr[k] = i
-            visited[i] = True
-            visited[i] = False
             func(k+1)
 
 func(0)
