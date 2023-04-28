0e0d23e805246ce3dc252d23e7754c3150131540
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-04-28 21:42:01 +0900

    [D3] Title: N-Queen, Time: 291 ms, Memory: 64,356 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -2,31 +2,29 @@
 T = int(input())
 
 for i in range(1, T+1):
+    
     n = int(input())
-    cnt = 0
-    isused_col = [0] * (n + 1)  
-    isused_dia1 = [0] * (2 * n) 
-    isused_dia2 = [0] * (2 * n) 
-
-    def func(curr):
-        global cnt
-        if curr == n:  # 퀸 n개를 놓는데 성공
-            cnt += 1
-            return
-
-        for i in range(n):
-            if isused_col[i] or isused_dia1[i + curr] or isused_dia2[curr - i + (n - 1)]:
-                continue
-
-            isused_col[i] = True
-            isused_dia1[i + curr] = True
-            isused_dia2[curr - i + n - 1] = True
-
-            func(curr + 1)
-            isused_col[i] = False
-            isused_dia1[i + curr] = False
-            isused_dia2[curr - i + n - 1] = False
-
+    
+    ans = 0
+    row = [0] * n
+    
+    def is_promising(x):
+        for i in range(x):
+            if row[i] == row[x] or abs(row[x] - row[i]) == abs(x - i):
+                return False
+        return True
+    
+    def func(x):
+        global ans
+        if x == n:
+            ans += 1
+            return 
+        
+        else:
+            for i in range(n):
+                row[x] = i
+                if is_promising(x):
+                    func(x + 1)
 
     func(0)
-    print(f"#{i} {cnt}")
+    print(f"#{i} {ans}")
