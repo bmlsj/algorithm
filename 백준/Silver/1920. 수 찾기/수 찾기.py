48bfea036b2d8cd15ff622ca6b9498c6571045a9
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-04-30 00:17:20 +0900

    [Silver IV] Title: 수 찾기, Time: 232 ms, Memory: 147812 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+nlist = set(map(int, input().split()))
+
+m = int(input())
+mlist = list(map(int, input().split()))
+
+for i in mlist:
+    if i in nlist:
+        print(1)
+    else:
+        print(0)
