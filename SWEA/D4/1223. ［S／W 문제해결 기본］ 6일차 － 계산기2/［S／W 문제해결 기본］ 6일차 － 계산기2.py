dfcda0d5aa513ceb2e1e09730252504d813e5ee7
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-01 23:05:35 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 159 ms, Memory: 58,524 KB -BaekjoonHub

---
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -0,0 +1,45 @@
+
+for tc in range(1,11):
+    T = int(input())
+    TC = list(input())
+
+    # 1. 후위 표기식으로 바꾸기
+    priority = {'*':2, '+':1}
+    postfix = []
+    stack = []
+    
+    for i in TC:
+        # 숫자면 결과에 넣기
+        if i != '*' and i != '+':
+            postfix.append(i)
+        else:
+            # 스택이 비어있으면
+            if not stack:
+                stack.append(i)
+            # 스택이 비어있지 않으면
+            else:
+                # stack[-1] 보다 우선순위가 낮다면 stack에 있는 연산자 다 빼내기
+                if priority[stack[-1]] >= priority[i]:
+                    while stack and priority[stack[-1]] >= priority[i]:
+                        postfix.append(stack.pop())
+                stack.append(i)
+    # 남아있는 연산자 모두 넣기
+    while stack:
+        postfix.append(stack.pop())
+
+    cal = []
+    for i in postfix:
+        if i != '+' and i != '*':
+            cal.append(int(i))
+
+        elif i == '*':
+            n1 = cal.pop()
+            n2 = cal.pop()
+            cal.append(n1 * n2)
+
+        elif i == '+':
+            n1 = cal.pop()
+            n2 = cal.pop()
+            cal.append(n1 + n2)
+
+    print(f"#{tc} {cal[0]}")
