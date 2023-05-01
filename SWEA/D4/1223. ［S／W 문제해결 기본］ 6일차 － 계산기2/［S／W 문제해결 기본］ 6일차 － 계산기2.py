76cd18fc3157bced58ed7274c94ec1a9c03fdb87
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-01 23:23:03 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 148 ms, Memory: 58,760 KB -BaekjoonHub

--- SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -1,27 +1,29 @@
 
-for tc in range(1,11):
+for tc in range(1, 11):
 
     T = int(input())
     TC = list(input())
 
     # 1. 후위 표기식으로 바꾸기
-    priority = {'*':2, '+':1}
+    priority = {'*': 2, '+': 1}
     postfix = []
     stack = []
+
     for i in TC:
-        # 숫자면 결과에 넣기
-        if i != '*' and i != '+':
+        if i != '*' and i != '+':  # 숫자일 경우
             postfix.append(i)
 
-        elif not stack:
-                stack.append(i)
-            # 스택이 비어있지 않으면
-        else:
-            # stack[-1] 보다 우선순위가 낮다면 stack에 있는 연산자 다 빼내기
-            if priority[stack[-1]] >= priority[i]:
-                while stack and priority[stack[-1]] >= priority[i]:
-                    postfix.append(stack.pop())
+        elif not stack:  # 스택이 비었을 경우
             stack.append(i)
+
+        else:  # 스택이 비어있지 않으면
+            # 현재 연산자가 stack의 마지막 연산자보다 우선순위가 낮다면,
+            # stack에 남아 있는 연산자를 postfix로
+            while stack and priority[stack[-1]] >= priority[i]:
+                postfix.append(stack.pop())
+
+            stack.append(i)
+
     # 남아있는 연산자 모두 넣기
     while stack:
         postfix.append(stack.pop())
