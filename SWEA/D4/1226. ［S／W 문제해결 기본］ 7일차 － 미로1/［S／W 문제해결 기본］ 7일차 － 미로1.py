e9d696b9d7a143ff17447f57a29f68d5d95774cb
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-02 16:19:35 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 124 ms, Memory: 54,320 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,36 +1,27 @@
-from collections import deque
-
 for i in range(1, 11):
 
     # 0: 길, 2: 출발점, 3: 도착점
     n = int(input())
     arr = [list(map(int, input())) for _ in range(16)]
+    ans = 0
 
-    q = deque()
+    def dfs(x, y):
+        global ans
+        arr[x][y] = 1
 
-    for j in range(16):
-        for k in range(16):
-            if arr[j][k] == 2:
-                q.append([j, k])
+        dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
+            if arr[nx][ny] == 3:
+                ans = 1
+                return
 
-    def bfs():
-
-        while q:
-            a, b = q.popleft()
-            arr[a][b] = 1
-
-            dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-            for i in range(4):
-                nx = a + dx[i]
-                ny = b + dy[i]
-
-                if arr[nx][ny] == 3:
-                    return 1
-
-                if 0 <= nx < 16 and 0 <= ny < 16 and arr[nx][ny] == 0:
-                    arr[nx][ny] = 1
-                    q.append([nx, ny])
+            if 0 <= nx < 16 and 0 <= ny < 16 and arr[nx][ny] == 0:
+                arr[nx][ny] = 1
+                dfs(nx, ny)
 
 
-    print(f"#{n} {1 if bfs() else 0}")
+    dfs(1, 1)
+    print(f"#{n} {1 if ans else 0}")
