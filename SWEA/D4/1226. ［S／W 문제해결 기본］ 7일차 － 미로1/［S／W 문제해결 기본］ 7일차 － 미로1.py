c03207044c0eaeb6b7e4045e025914e21c72f32a
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-02 15:21:28 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 141 ms, Memory: 59,712 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -5,7 +5,6 @@ for i in range(1, 11):
     # 0: 길, 2: 출발점, 3: 도착점
     n = int(input())
     arr = [list(map(int, input())) for _ in range(16)]
-    visited = [[False] * 16 for _ in range(16)]
 
     q = deque()
 
@@ -19,7 +18,7 @@ for i in range(1, 11):
 
         while q:
             a, b = q.popleft()
-            visited[a][b] = True
+            arr[a][b] = 1
 
             dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
             for i in range(4):
@@ -29,8 +28,8 @@ for i in range(1, 11):
                 if arr[nx][ny] == 3:
                     return 1
 
-                if 0 <= nx < 16 and 0 <= ny < 16 and not visited[nx][ny] and arr[nx][ny] == 0:
-                    visited[nx][ny] = True
+                if 0 <= nx < 16 and 0 <= ny < 16 and arr[nx][ny] == 0:
+                    arr[nx][ny] = 1
                     q.append([nx, ny])
 
 
