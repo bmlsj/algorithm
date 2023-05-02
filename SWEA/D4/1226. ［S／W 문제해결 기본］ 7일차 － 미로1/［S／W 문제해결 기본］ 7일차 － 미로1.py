96374e0cbe7d71c45d4bae3ff106c9989fa8a9f7
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-02 15:15:01 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 149 ms, Memory: 57,916 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+for i in range(1, 11):
+
+    # 0: 길, 2: 출발점, 3: 도착점
+    n = int(input())
+    arr = [list(map(int, input())) for _ in range(16)]
+    visited = [[False] * 16 for _ in range(16)]
+
+    q = deque()
+
+    for j in range(16):
+        for k in range(16):
+            if arr[j][k] == 2:
+                q.append([j, k])
+
+
+    def bfs():
+
+        while q:
+            a, b = q.popleft()
+            visited[a][b] = True
+
+            dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+            for i in range(4):
+                nx = a + dx[i]
+                ny = b + dy[i]
+
+                if arr[nx][ny] == 3:
+                    return 1
+
+                if 0 <= nx < 16 and 0 <= ny < 16 and not visited[nx][ny] and arr[nx][ny] == 0:
+                    visited[nx][ny] = True
+                    q.append([nx, ny])
+
+
+    print(f"#{n} {1 if bfs() else 0}")
