fbb820133d9411f56a67389b51a7fa9b51f900f5
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-04 13:25:14 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 3416 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,5 +1,5 @@
 n = int(input())
-INF = 1000000
+INF = 1005000
 
 
 def is_prime(n):
@@ -12,14 +12,11 @@ ans = 0
 for i in range(n, INF+1):
     if i == 1:
         continue
-        
+
     if is_prime(i):
         num = str(i)
         if num == num[::-1]:
             ans = int(num)
             break
-
-if ans == 0:     # 입력값이 최대 값 100만일 경우
-    ans = 1003001    # 100만 이상이면서 팰림드롬 및 소수일 경우를 적용
 
 print(ans)
