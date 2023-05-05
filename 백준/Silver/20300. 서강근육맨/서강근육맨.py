7abdcdfa21d3c60aff4fa7f99fe24396dfd79e9e
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-05 22:40:42 +0900

    [Silver III] Title: 서강근육맨, Time: 48 ms, Memory: 32464 KB -BaekjoonHub

---
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.py
@@ -0,0 +1,18 @@
+n = int(input())
+muscle = list(map(int, input().split()))
+muscle.sort()
+ans = []
+
+if n % 2:
+    ans.append(muscle[-1])
+    muscle.pop()
+    for i in range(n // 2):
+        ans.append(muscle[i] + muscle.pop())
+
+    print(max(ans))
+
+else:
+    for i in range(n // 2):
+        ans.append(muscle[i] + muscle.pop())
+
+    print(max(ans))
