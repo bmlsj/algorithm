5726b525f8e324c326d3dc4f74210669c566c06b
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-09 16:29:03 +0900

    [Silver III] Title: 01타일, Time: 320 ms, Memory: 69788 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,13 +1,13 @@
-# 1과 00 타일만 남음
 import sys
-
 input = sys.stdin.readline
 n = int(input())
-d = [0] * (n + 1)
-d[0] = 1
-d[1] = 2
+
+# 1과 00만 사용 가능
+dp = [0] * (n + 1)
+dp[0] = 1  # 1
+dp[1] = 2  # 00, 11
 
 for i in range(2, n + 1):
-    d[i] = (d[i - 1] + d[i - 2]) % 15746
+    dp[i] = (dp[i - 1] + dp[i - 2])  % 15746
 
-print(d[-2])
+print(dp[n-1])
