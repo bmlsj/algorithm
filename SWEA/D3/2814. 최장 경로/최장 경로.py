59fcc798cf6684d71a2c97022bc6b129c1bdb326
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-16 21:50:39 +0900

    [D3] Title: 최장 경로, Time: 244 ms, Memory: 63,916 KB -BaekjoonHub

---
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+t = int(input())
+
+for k in range(1, t + 1):
+    n, m = map(int, input().split())
+
+    # 정점 개수 : n / 간선 개수 : m
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+        graph[a].sort()
+        graph[b].sort()
+
+
+    def dfs(v, cnt):
+        global ans
+
+        if ans < cnt:
+            ans = cnt
+
+        for next in graph[v]:
+            if not visited[next]:
+                visited[v] = True
+                dfs(next, cnt + 1)
+                visited[v] = False
+
+
+    cnt = 1
+    visited = [False] * (n + 1)
+
+    ans = 0
+    for a in range(1, n + 1):
+        visited[a] = True
+        dfs(a, 1)
+        visited[a] = False
+
+    print(f"#{k} {ans}")
