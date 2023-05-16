b438de6fc139a61cf23385d83f0462542fb247c3
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-16 22:12:21 +0900

    [D3] Title: 최장 경로, Time: 209 ms, Memory: 63,332 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 t = int(input())
 
 for k in range(1, t + 1):
@@ -18,21 +16,16 @@ for k in range(1, t + 1):
 
     def dfs(v, cnt):
         global ans
-
-        if ans < cnt:
-            ans = cnt
-
+        ans = max(cnt, ans)
         for i in graph[v]:
             if not visited[i]:
-                visited[v] = True
+                visited[i] = True
                 dfs(i, cnt + 1)
-                visited[v] = False
+                visited[i] = False
 
-
-    cnt = 1
-    visited = [False] * (n + 1)
 
     ans = 0
+    visited = [0] * (n + 1)
     for a in range(1, n + 1):
         visited[a] = True
         dfs(a, 1)
