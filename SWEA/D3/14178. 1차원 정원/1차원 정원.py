c8033d9464cfd3bd6d1b52d9d0629ed5ee012c95
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-20 18:14:09 +0900

    [D3] Title: 1차원 정원, Time: 1,326 ms, Memory: 64,884 KB -BaekjoonHub

--- SWEA/D3/14178. 1차원 정원/1차원 정원.py
+++ SWEA/D3/14178. 1차원 정원/1차원 정원.py
@@ -2,15 +2,9 @@ t = int(input())
 
 for i in range(1, t + 1):
     n, d = map(int, input().split())
-    # [x - d, x + d]
+    cnt = n // (2 * d + 1)
 
-    # cnt = n // (2 * d + 1)
-    cnt = 0
-    while n >= d:
-
+    if n % (2 * d + 1) > 0:
         cnt += 1
-        n -= (2 * d + 1)
 
-    if n > 0:
-        cnt += 1
     print(f"#{i} {cnt}")
