61b167fc892ac5ed0ecaee5f17d78b0b935fa57f
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-05-21 00:49:15 +0900

    [D3] Title: 교환학생, Time: 792 ms, Memory: 64,364 KB -BaekjoonHub

---
+++ SWEA/D3/13038. 교환학생/교환학생.py
@@ -0,0 +1,28 @@
+t = int(input())
+
+for i in range(1, t + 1):
+
+    n = int(input())
+    days = list(map(int, input().split()))
+
+    onelist = []
+    for a in range(len(days)):
+        if days[a] == 1:
+            onelist.append(a)
+
+    ans = 10**6
+    cntlist = []
+    for start in onelist:
+        day, cnt = 0, 0
+        s = start
+        while cnt != n:
+            for a in range(start, 7):
+                if days[a] == 1:
+                    cnt += days[a]
+                day += 1
+                if cnt >= n:
+                    break
+            start = 0
+        cntlist.append(day)
+
+    print(f"#{i} {min(cntlist)}")
