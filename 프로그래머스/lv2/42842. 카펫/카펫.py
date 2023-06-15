bcb3b4c1776e82db54b9d59fb471584e3df323f9
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-15 22:23:20 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -2,12 +2,7 @@ def solution(brown, yellow):
     
     total = brown + yellow
     
-    ans = []
+
     for i in range(1, total // 2 + 1):
         if total % i == 0 and (i*2 + (total//i - 2)*2) == brown:
-            ans.append(i)
-    
-    if len(ans) == 1:
-        return [ans[0], ans[0]]
-    else:
-        return sorted(ans, reverse=True)
+            return [total//i, i]
