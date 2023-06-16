30631a6210d2f94293d22998dd398fcf80857cfc
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-16 23:03:23 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -3,19 +3,12 @@ def solution(k, score):
     legend = []
     ans = []
     for i in range(1, len(score) + 1):
-        legend.sort()
-        if i <= k:
-            if len(legend) == k:
-                legend.pop(0)
-            legend.append(score[i-1])
         
-        else:
-            if min(legend) < score[i-1]:
-                if len(legend) == k:
-                    legend.pop(0)
-                legend.append(score[i-1])
+        legend.append(score[i-1])
         
-        # print(legend)
+        if len(legend) > k:
+            legend.remove(min(legend))
+
         ans.append(min(legend))
         
     return ans
