06ab733be3fa737ec7cdfe2564503333bd5993e6
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-17 22:16:00 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -14,22 +14,21 @@ def solution(str1, str2):
     j = 0
     
     if len(s1) == 0 and len(s2) == 0:  # 모두 공집합일 경우
-        j = 1
+        return 65536
         
-    else:
-        inter = set(s1) | set(s2)
-        intersection = []
-        for i in inter:
-            for _ in range(min(s1.count(i), s2.count(i))):
-                intersection.append(i)
+    
+    inter = set(s1) | set(s2)
+    intersection = []
+    for i in inter:
+        for _ in range(min(s1.count(i), s2.count(i))):
+            intersection.append(i)
         
-        uni = set(s1) | set(s2)
-        union = []
-        for i in uni:
-            for _ in range(max(s1.count(i), s2.count(i))):
-                union.append(i)
+    uni = set(s1) | set(s2)
+    union = []
+    for i in uni:
+        for _ in range(max(s1.count(i), s2.count(i))):
+            union.append(i)
 
-        j = len(intersection) / len(union)
+    j = len(intersection) / len(union)
 
-        
     return int(j * 65536)
