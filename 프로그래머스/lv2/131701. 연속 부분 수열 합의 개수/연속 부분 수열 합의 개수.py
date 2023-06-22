7bb938c17ba20cdf4f0c45961cb9d990c86264c2
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-22 22:34:54 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5059.81 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,11 +1,3 @@
-def is_prime(n):
-    
-    for i in range(2, n // 2 + 1):
-        if n % i == 0:
-            return False
-    
-    return True
-
 def solution(elements):
     
     numlist = set()
