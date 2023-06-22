b84107d6910812c4de948d10d3bbdf174d8e607b
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-22 16:19:18 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,7 +2,7 @@ def solution(food):
     
     result = ""
     for i in range(1, len(food)):
-        if food[i] // 2 != 0:
+        if food[i] // 2:
             for _ in range(food[i] // 2):
                 result += str(i)
     
