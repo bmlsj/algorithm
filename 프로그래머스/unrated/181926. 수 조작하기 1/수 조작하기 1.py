efa7a828bff2fb303a5aeac5c0efa16c33b2cd22
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-24 18:06:28 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.60 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -11,3 +11,7 @@ def solution(n, control):
             n -= 10
             
     return n
+
+# def solution(n, control):
+#     key = dict(zip(['w','s','d','a'], [1,-1,10,-10]))
+#     return n + sum([key[c] for c in control])
