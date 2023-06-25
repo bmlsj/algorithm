375f579ee83728606cc956fbd6633880050f014d
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-25 23:19:56 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.py
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.py
@@ -1,4 +1,17 @@
+def split(string):
+    
+    split_str = []
+    tmp = ''
+    for s in string:
+        if s == ' ':
+            split_str.append(tmp)
+            tmp = ''
+            continue
+        tmp += s
+    
+    split_str.append(tmp)
+    return split_str
+        
 def solution(my_string):
     
-    string = my_string.split()
-    return string
+    return split(my_string)
