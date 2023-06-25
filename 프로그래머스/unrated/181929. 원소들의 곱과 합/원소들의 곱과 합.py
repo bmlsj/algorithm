3f3f8ba4caddaeb49143ea3fc0ce2938eddc030a
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-25 23:31:00 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,7 +1,15 @@
+def Sum(numlist):
+    
+    total = 0
+    for n in numlist:
+        total += n
+    
+    return total
+
 def solution(num_list):
     
     mult = 1
     for n in num_list:
         mult *= n
     
-    return 1 if mult < sum(num_list) ** 2 else 0
+    return 1 if mult < Sum(num_list) ** 2 else 0
