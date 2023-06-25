ccd7fb04fb7205c3cec53e02e5f1f67e6e6eb70c
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-25 23:13:44 +0900

    [unrated] Title: 문자열의 앞의 n글자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.py
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.py
@@ -1,2 +1,10 @@
+def split(string, num):
+
+    ans = ''
+    for s in range(len(string)):
+        ans += string[s]
+        if s == num - 1:
+            return ans
+        
 def solution(my_string, n):
-    return my_string[:n]
+    return split(my_string, n)
