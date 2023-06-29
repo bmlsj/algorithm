b3867f3e3ba5ec3047d116ee12382aa84be2a871
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-06-29 14:10:45 +0900

    [unrated] Title: A 강조하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
@@ -1,12 +1,3 @@
 def solution(myString):
     
-    ans = ''
-    for s in myString:
-        if s == 'a':
-            ans += s.upper()
-        elif s == 'A':
-            ans += s
-        else:
-            ans += s.lower()
-
-    return ans
+    return myString.lower().replace('a', 'A')
