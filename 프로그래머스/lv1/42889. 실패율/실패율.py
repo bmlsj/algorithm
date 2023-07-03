aa2a2a1b648bc347bd65591ed3e7f309f49d7a43
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-07-03 19:42:03 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,21 +1,16 @@
 def solution(N, stages):
-    # 실패율 : 스테이지에 도달 but 클리어X / 스테이지에 도달
-    stages.sort()
+    
+    # 실패율 : 스테이지에 도달 but 클리어X / 스테이지에 도달 
     ans = []
-    for i in range(1, N + 1):
+    for i in range(1, N+1):
         cnt, fail = 0, 0
         for stage in stages:
             if stage >= i:
                 cnt += 1
             if stage == i:
                 fail += 1
-
-        if fail == len(stages) - 1:
-            ans.append([i, 1])
-        elif cnt == 0:
-            ans.append([i, 0])
-        else:
-            ans.append([i, fail / cnt])
+        
+        ans.append([i, fail/cnt])
     
-    ans = sorted(ans, key=lambda t: -t[1])
-    return [ans[i][0] for i in range(len(ans))] 
+    ans = sorted(ans, key=lambda t:-t[1])
+    return [ans[i][0] for i in range(len(ans))]
