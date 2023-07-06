3c8a20333acfb88d52363903b4de830ab64b172c
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-07-06 22:20:47 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 1188 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -1,17 +1,23 @@
 import sys
+
 input = sys.stdin.readline
-
 d, k = map(int, input().split())
-dp = [0] * d
 
-for i in range(1, 100000):
-    for j in range(1, 100000):
+dp = [0] * d
+check = False
+
+for i in range(1, k):
+    for j in range(1, k):
         dp[0], dp[1] = i, j
         for a in range(2, d):
             dp[a] = dp[a - 1] + dp[a - 2]
-
+            
         if dp[-1] == k:
-            print(dp[0])
-            print(dp[1])
-            exit()
+            check = True
+            break
+    if check:
+        break
 
+
+print(dp[0])
+print(dp[1])
