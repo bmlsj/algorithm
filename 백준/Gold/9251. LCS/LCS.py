052777fd5f769ec2a5d2ee199036f2d9c747b001
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-07-07 14:26:51 +0900

    [Gold V] Title: LCS, Time: 248 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+str1 = input().rstrip()
+str2 = input().rstrip()
+
+cache = [0] * len(str2)
+
+for i in range(len(str1)):
+    cnt = 0
+    for j in range(len(str2)):
+        if cnt < cache[j]:
+            cnt = cache[j]
+        elif str1[i] == str2[j]:
+            cache[j] = cnt + 1
+
+print(max(cache))
