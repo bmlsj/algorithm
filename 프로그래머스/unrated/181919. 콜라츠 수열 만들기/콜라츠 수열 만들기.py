bf04bacf89d1ee826f8b42254a43c981c6f12b72
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-07-09 23:03:01 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    
+    ans = [n]
+    while n != 1:
+        if n % 2 != 0:
+            n = 3*n + 1
+            ans.append(n)
+        else:
+            n //= 2
+            ans.append(n)
+    
+    return ans
