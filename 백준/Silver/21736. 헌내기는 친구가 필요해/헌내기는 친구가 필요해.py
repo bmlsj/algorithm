09428981c097bc1b909ac6a4f0a5fa5d22d1db63
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-07-10 17:05:06 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 452 ms, Memory: 40500 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,39 +1,42 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(360000)
+
 
 n, m = map(int, input().split())
 campus = [list(input()) for _ in range(n)]
-
-
-def dfs(x, y):
-
-    global cnt
-    visited[x][y] = True
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if nx < 0 or nx >= n or ny < 0 or ny >= m:
-            continue
-
-        if not visited[nx][ny]:
-            visited[nx][ny] = True
-            if campus[nx][ny] == 'O':
-                dfs(nx, ny)
-
-            elif campus[nx][ny] == 'P':
-                dfs(nx, ny)
-                cnt += 1
-
-
-cnt = 0
+queue = deque()
 visited = [[False] * m for _ in range(n)]
+
 for i in range(n):
     for j in range(m):
         if campus[i][j] == 'I':
-            dfs(i, j)
+            queue.append([i, j])
             break
 
-print('TT' if not cnt else cnt)
+def bfs():
+
+    global cnt
+    while queue:
+        a, b = queue.popleft()
+
+        dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+        for idx in range(4):
+            nx = a + dx[idx]
+            ny = b + dy[idx]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if not visited[nx][ny]:
+                visited[nx][ny] = True
+                if campus[nx][ny] == 'O':
+                    queue.append([nx, ny])
+
+                elif campus[nx][ny] == 'P':
+                    queue.append([nx, ny])
+                    cnt += 1
+
+cnt = 0
+bfs()
+print(cnt if cnt else 'TT')
