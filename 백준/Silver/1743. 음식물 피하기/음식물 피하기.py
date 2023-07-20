c0fb428f209a7b4ebd60f12cd47057142bc2a4e7
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-07-21 00:22:40 +0900

    [Silver I] Title: 음식물 피하기, Time: 60 ms, Memory: 33220 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,49 +1,35 @@
 import sys
-from collections import deque
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
 arr = [[0] * m for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+for _ in range(k):
+    r, c = map(int, input().split())
+    arr[r-1][c-1] = 1
 
 
-def bfs(a, b, cnt):
+def dfs(x, y, cnt):
 
-    queue = deque()
-    queue.append([a, b])
-
-    visited[a][b] = True
+    visited[x][y] = True
     cnt += 1
 
-    while queue:
-        a, b = queue.popleft()
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-        dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-        for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-
-            if arr[nx][ny] and not visited[nx][ny]:
-                visited[nx][ny] = True
-                cnt += bfs(nx, ny, 0)
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and arr[nx][ny]:
+            visited[nx][ny] = True
+            cnt += dfs(nx, ny, 0)
 
     return cnt
 
-
-queue = deque()
-for _ in range(k):
-    r, c = map(int, input().split())
-    r, c = r - 1, c - 1
-    arr[r][c] = 1
-
-visited = [[0] * m for _ in range(n)]
 ans = 0
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 1:
-            ans = max(ans, bfs(i, j, 0))
+        if arr[i][j]:
+            ans = max(ans, dfs(i, j, 0))
 
 print(ans)
