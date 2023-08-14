39cf00b320fea18cd6202ee6de5be458489f3bdc
bmlsj <97381846+bmlsj@users.noreply.github.com>
2023-08-14 18:22:15 +0900

    [Silver II] Title: 스타트와 링크, Time: 5292 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,11 +1,9 @@
 import sys
-
 input = sys.stdin.readline
 
 n = int(input())
 # 능력치 담은 리스트
 powerArr = [list(map(int, input().split())) for _ in range(n)]
-numlist = [i for i in range(n)]
 ans = 10001
 visited = [False] * n
 
@@ -14,21 +12,22 @@ visited = [False] * n
 def combination(cnt, depth, begin):
     global ans
     if cnt == depth:
-        power = 0
+        start_power, link_power = 0, 0
         for i in range(n):
             for j in range(n):
                 if visited[i] and visited[j]:
-                    power += powerArr[i][j]
+                    start_power += powerArr[i][j]
                 elif not visited[i] and not visited[j]:
-                    power -= powerArr[i][j]
+                    link_power += powerArr[i][j]
 
-        ans = min(ans, abs(power))
+        ans = min(ans, abs(start_power - link_power))
         return
 
     for idx in range(begin, n):
-        visited[idx] = True
-        combination(cnt + 1, depth, idx + 1)
-        visited[idx] = False
+        if not visited[idx]:
+            visited[idx] = True
+            combination(cnt + 1, depth, idx + 1)
+            visited[idx] = False
 
 
 combination(0, n // 2, 0)
