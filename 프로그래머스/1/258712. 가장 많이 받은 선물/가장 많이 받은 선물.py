4e23067ae07e70dc18785b7393f84e8f8c35a49b
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-05-10 17:13:11 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 26.54 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -0,0 +1,38 @@
+def solution(friends, gifts):
+    
+    jiso = [0] * len(friends)
+    getgift = [[0] * len(friends) for _ in range(len(friends))]     
+    
+    for gift in gifts:
+            a, b = gift.split(" ")
+            getgift[friends.index(a)][friends.index(b)] += 1
+    
+    for i in range(len(friends)):
+        name = friends[i]
+        
+        for gift in gifts:
+            a, b = gift.split(" ")
+            # 선물 지수
+            if a == name:
+                jiso[i] += 1
+            elif b == name:
+                jiso[i] -= 1
+    
+    gift = [0] * len(friends)
+    for i in range(len(getgift)):
+        for j in range(len(getgift)):
+            if i == j:
+                continue
+            
+            elif getgift[i][j] < getgift[j][i]:
+                gift[j] += 1
+            
+            elif getgift[i][j] == getgift[j][i]:
+                if jiso[i] < jiso[j]:
+                    gift[j] += 1
+                elif jiso[i] == jiso[j]:
+                    continue
+    
+
+    # print(gift)
+    return max(gift)
