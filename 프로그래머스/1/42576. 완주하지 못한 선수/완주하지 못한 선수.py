8bb3a8b74e47dd9127f4552dee3cc9c570b1ec4f
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-05-10 18:33:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.58 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,8 @@
-from collections import defaultdict
+from collections import Counter
 
 def solution(participant, completion):
 
-    p_dict = defaultdict(int)
-    for p in participant:
-        p_dict[p] += 1
+    part = Counter(participant)
+    comp = Counter(completion)
 
-    p_dict = dict(p_dict)
-
-    for c in completion:
-        p_dict[c] -= 1
-
-    for k, v in p_dict.items():
-        if v > 0:
-            return k
+    return list((part - comp).keys())[0]
