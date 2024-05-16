f6691544dfa2ab713200f4e008cc8d0e2027d2ab
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-05-16 16:18:13 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,24 @@
+def solution(n, lost, reserve):
+    s = set()
+    reserve.sort()
+    lost.sort()
+
+    for i in range(1, n + 1):
+        if i in lost:
+            continue
+        s.add(i)
+
+    for i in reserve:
+        if i in lost:
+            s.add(i)
+        elif i - 1 in lost and i - 1 not in s:
+            s.add(i - 1)
+        elif i + 1 in lost and i + 1 not in s:
+            s.add(i + 1)
+
+    for i in reserve:
+        if i not in s:
+            s.add(i)
+
+    print(s)
+    return len(s)
