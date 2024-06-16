52b745fa113171c15396ac2ad0282bfbc463424f
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-06-16 17:05:45 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,14 +1,7 @@
 def solution(spell, dic):
-    
-    dic_cnt = [0] * len(dic)
-    
-    for i in range(len(dic)):
-        for n in spell:
-            if dic[i].count(n):
-                dic_cnt[i] += 1
-                
-    for cnt in dic_cnt:
-        if cnt == len(spell):
+
+    for s in dic:
+        if not set(spell) - set(s):
             return 1
     
     return 2
