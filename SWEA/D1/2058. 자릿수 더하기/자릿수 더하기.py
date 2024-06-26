58beed3c217c70d3fc19beb448b0ad3a68a9a995
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-06-26 19:18:16 +0900

    [D1] Title: 자릿수 더하기, Time: 97 ms, Memory: 42,132 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,19 @@
-num = input()
-ans = 0
-for n in num:
-    ans += int(n)
-print(ans)
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String num = sc.nextLine();
+        int ans = 0;
+
+        for (String n: num.split("")){
+            ans += Integer.parseInt(n);
+        }
+
+        System.out.println(ans);
+
+    }
+}
