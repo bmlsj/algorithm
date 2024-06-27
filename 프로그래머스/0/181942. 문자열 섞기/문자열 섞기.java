cc7625215ed9f26f489208e46737ea06d352bc16
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-06-27 16:17:24 +0900

    [level 0] Title: 문자열 섞기, Time: 9.05 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,18 +1,13 @@
-import java.util.Arrays;
-import java.util.ArrayList;
 
 class Solution {
     public String solution(String str1, String str2) {
-        StringBuilder answer = new StringBuilder();
-        ArrayList<String> s1 = new ArrayList<>(Arrays.asList(str1.split("")));
-        ArrayList<String> s2 = new ArrayList<>(Arrays.asList(str2.split("")));
+        String ans = "" ;
 
-        for (int i = 0; i < s1.size(); i++){
-            answer.append(s1.get(i));
-            answer.append(s2.get(i));
+        for (int i = 0; i < str1.length(); i++) {
+            ans += (str1.charAt(i));
+            ans += (str2.charAt(i));
         }
 
-        System.out.println(answer);
-        return answer.toString();
+        return ans;
     }
 }
