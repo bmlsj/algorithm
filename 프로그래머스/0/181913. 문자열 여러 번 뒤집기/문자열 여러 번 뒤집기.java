98e2bda85e6dbc03b85f3e80e851152f73999182
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-06-28 15:33:22 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 31.23 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,30 +1,16 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
+    
+
         for (int[] index : queries) {
+            StringBuilder sb = new StringBuilder();
             int s = index[0], e = index[1];
-            my_string = reverse(my_string, s, e);
+            for (int i = s; i <= e; i++){
+                sb.append(my_string.charAt(i));
+            }
+            my_string = my_string.substring(0, s) + sb.reverse().toString() + my_string.substring(e+1);
         }
 
-        System.out.println(my_string);
         return my_string;
     }
-    
-    public static String reverse(String word, int s, int e) {
-
-        char[] charArr = word.toCharArray();
-
-        while (s < e) {
-            swap(charArr, s, e);
-            s++;
-            e--;
-        }
-        return new String(charArr);
-    }
-
-    public static void swap(char[] charArr, int s, int e) {
-        char tmp = charArr[s];
-        charArr[s] = charArr[e];
-        charArr[e] = tmp;
-    }
-
 }
