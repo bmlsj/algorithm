472c3ba13c94c47c2352cc8911dd987b5fb6bc8f
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-06-28 17:18:32 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.29 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,65 +1,34 @@
+import java.util.*;
 class Solution {
-        // Method to handle the case where exactly one number is different
-    public static int oneDiff(int a, int b, int c, int d) {
-        if (a != d && b == c && b == d) {
-            return (int) Math.pow(10 * b + a, 2);
-        } else if (b != d && a == c && a == d) {
-            return (int) Math.pow(10 * a + b, 2);
-        } else if (c != d && a == b && a == d) {
-            return (int) Math.pow(10 * a + c, 2);
-        } else if (d != c && a == b && a == c) {
-            return (int) Math.pow(10 * a + d, 2);
-        }
-        return 0;
-    }
-
-    public static int twoDifftwoSame(int a, int b, int c, int d) {
-        if ((a == b && c == d)) {
-            return (a + c) * Math.abs(a - c);
-        } else if ((a == c && b == d) || (b == c && a == d)){
-            return (a + b) * Math.abs(a - b);
-        }
-        return 0;
-    }
-
-    // Method to handle the case where there are two equal numbers and two different numbers
-    public static int twoDiff(int a, int b, int c, int d) {
-        if (a == b && c != d) {
-            return c * d;
-        } else if (a == c && b != d) {
-            return b * d;
-        } else if (a == d && b != c) {
-            return b * c;
-        } else if (b == c && a != d) {
-            return a * d;
-        } else if (b == d && a != c) {
-            return a * c;
-        } else if (c == d && a != b) {
-            return a * b;
-        }
-        return 0;
-    }
-
-    // Solution method to determine the result based on the given rules
     public static int solution(int a, int b, int c, int d) {
-        // 모두 같으면, 1111 * p
-        if (a == b && b == c && c == d) {
-            return 1111 * a;
+        HashSet<Integer> arrSet = new HashSet<>(Arrays.asList(a, b, c, d));
+        List<Integer> arrList = new ArrayList<>(Arrays.asList(a, b, c, d));
+        List<Integer> arrSetList = new ArrayList<>(arrSet);
+
+        if (arrSet.size() == 4) {   // 값이 모두 다를 때, 최솟값
+            return Collections.min(arrSetList);
+        } else if (arrSet.size() == 3) {  // 값이 2개만 같을 때, 다른 값 q * r
+            int ans = 1;
+            for (Integer num : arrSet) {
+                int tmp = num;
+                if (Collections.frequency(arrList, tmp) == 1) {
+                    ans *= tmp;
+                }
+            }
+            return ans;
+        } else if (arrSet.size() == 2) {  // 값이 2개씩 같거나 3개가 같을 때
+            int n1 = arrSetList.get(0);
+            int n2 = arrSetList.get(1);
+            if (Collections.frequency(arrList, n1) == 2 && Collections.frequency(arrList, n2) == 2) {
+                return (n1 + n2) * Math.abs(n1 - n2);
+            } else {
+                if (Collections.frequency(arrList, n1) == 3) return (int) Math.pow(10 * n1 + n2, 2);
+                else return (int) Math.pow(10 * n2 + n1, 2);
+
+            }
+        } else if (arrSet.size() == 1) {   // 값이 4개 같을 때
+            return 1111 * arrList.get(0);
         }
-
-        // 모두 다르면, 가장 작은 숫자만큼
-        if (a != b && a != c && a != d && b != c && b != d && c != d) {
-            return Math.min(Math.min(a, b), Math.min(c, d));
-        }
-
-        // Check for other cases
-        int result = oneDiff(a, b, c, d);
-        if (result != 0) return result;
-
-        result = twoDiff(a, b, c, d);
-        if (result != 0) return result;
-
-        result = twoDifftwoSame(a, b, c, d);
-        return result;
+        return -1;
     }
 }
