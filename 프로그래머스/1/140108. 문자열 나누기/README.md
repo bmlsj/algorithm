f729a93c8cc2fb1102d8824593a2a09657e12ebb
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-06-29 15:54:07 +0900

    [level 1] Title: 문자열 나누기, Time: 0.79 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/README.md
+++ 프로그래머스/1/140108. 문자열 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 나누기 - 140108 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/140108) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.66 ms
+메모리: 10.3 MB, 시간: 0.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 29일 15:49:09
+2024년 06월 29일 15:54:05
 
 ### 문제 설명
 
--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,27 +1,14 @@
 def solution(s):
+    ans = 0
+    same, diff = 0, 0
+    check = ''
+    for c in s:
+        if same == diff:
+            ans += 1
+            check = c
 
-    idx = 0
-    diff, same = 0, 1
-    clist = []
-
-    while idx < len(s):
-        diff, same = 0, 1
-        c = s[idx]
-        start = s[idx]
-
-        for i in range(idx + 1, len(s)):
-            if diff != same and s[i] == start:
-                same += 1
-                c += s[i]
-
-            elif diff != same and s[i] != start:
-                diff += 1
-                c += s[i]
-
-            if diff == same:
-                break
-
-        idx += (same + diff)
-        clist.append(c)
-
-    return len(clist)
+        if c == check:
+            same += 1
+        else:
+            diff += 1
+    return ans
