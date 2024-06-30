ef33a376bccbfc96d856971cf2901aefbd907bf7
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-06-30 23:56:30 +0900

    [D2] Title: 숫자 배열 회전, Time: 100 ms, Memory: 43,800 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -9,32 +9,14 @@ def rotate90(matrix, n):
     return newMap
 
 
-def rotate180(matrix, n):
-    newMap = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            newMap[i][j] = matrix[n - 1 - i][n - 1 - j]
-
-    return newMap
-
-
-def rotate270(matrix, n):
-    newMap = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            newMap[i][j] = matrix[j][n - 1 - i]
-
-    return newMap
-
-
 T = int(input())
 for t in range(T):
 
     n = int(input())
     matrix = [list(map(int, input().split())) for _ in range(n)]
     map90 = rotate90(matrix, n)
-    map180 = rotate180(matrix, n)
-    map270 = rotate270(matrix, n)
+    map180 = rotate90(map90, n)
+    map270 = rotate90(map180, n)
 
     print(f"#{t+1}")
     for i in range(n):
