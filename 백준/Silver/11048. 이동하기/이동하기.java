f759b2b573d44a29ebf9dcd30684e9821a0c5bc1
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-09-10 16:06:29 +0900

    [Silver II] Title: 이동하기, Time: 524 ms, Memory: 94760 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -1,52 +1,46 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Map;
-import java.util.Queue;
 
 public class Main {
 
-	private static int n, m;
-	private static int[][] map;
-	private static int[][] dp;
+    private static int n, m;
+    private static int[][] map;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = in.readLine().split(" ");
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String[] split = in.readLine().split(" ");
+        n = Integer.parseInt(split[0]);
+        m = Integer.parseInt(split[1]);
 
-		n = Integer.parseInt(split[0]);
-		m = Integer.parseInt(split[1]);
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            split = in.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(split[j]);
+            }
+        }
 
-		map = new int[n][m];
+        // 1D DP 배열 생성
+        long[] dp = new long[m];
 
-		for (int i = 0; i < n; i++) {
-			split = in.readLine().split(" ");
-			for (int j = 0; j < m; j++) {
-				map[i][j] = Integer.parseInt(split[j]);
-			}
-		}
+        // 초기화: 첫 번째 행 처리
+        dp[0] = map[0][0];
+        for (int j = 1; j < m; j++) {
+            dp[j] = dp[j - 1] + map[0][j];
+        }
 
-		dp = new int[n][m];
-		dp[0][0] = map[0][0];
+        // DP 배열 업데이트
+        for (int i = 1; i < n; i++) {
+            // 현재 행의 첫 번째 열 초기화
+            dp[0] += map[i][0];
+            for (int j = 1; j < m; j++) {
+                dp[j] = Math.max(dp[j], dp[j - 1]) + map[i][j];
+            }
+        }
 
-		for (int i = 1; i < n; i++) {
-			dp[i][0] = dp[i - 1][0] + map[i][0];
-		}
-
-		for (int j = 1; j < m; j++) {
-			dp[0][j] = dp[0][j - 1] + map[0][j];
-		}
-
-		// DP 테이블 채우기
-		for (int i = 1; i < n; i++) {
-			for (int j = 1; j < m; j++) {
-				dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + map[i][j];
-			}
-		}
-		System.out.println(dp[n - 1][m - 1]);
-
-	}
-
+        // 결과 출력
+        System.out.println(dp[m - 1]);
+    }
 }
