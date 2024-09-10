7b72ca04aa702bb05d679b3f1f2b804176890d34
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-09-10 21:47:53 +0900

    [Silver I] Title: 숨바꼭질, Time: 132 ms, Memory: 17512 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -5,60 +5,64 @@ import java.util.Queue;
 
 public class Main {
 
+	private static int n, k;
 	private static boolean[] visited;
-	private static int[] arr;
 	private static int INF = 100001;
 
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
 		String[] split = in.readLine().split(" ");
-		int n = Integer.parseInt(split[0]);
-		int k = Integer.parseInt(split[1]);
+		n = Integer.parseInt(split[0]);
+		k = Integer.parseInt(split[1]);
 
-		// x => 걷는다면 x-1, x+1
-		// 순간 이동시 , x => 2x
-		arr = new int[INF];
 		visited = new boolean[INF];
 
-		int res = bfs(n, k);
-		System.out.println(res);
+		int ans = find(n);
+		if (n == k) {
+			ans = 0;
+		}
+		System.out.println(ans);
+		// System.out.println(Arrays.toString(visited));
 
 	}
 
-	private static int bfs(int n, int k) {
+	private static int[] dx = { -1, 1 };
 
-		Queue<Integer> queue = new ArrayDeque<>();
+	private static int find(int v) {
 
-		queue.offer(n);
-		visited[n] = true;
-
-		int curr = 0;
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] { v, 0 });
+		visited[v] = true;
 
 		while (!queue.isEmpty()) {
-			curr = queue.poll();
-			int[] dx = new int[] { -1, 1, curr };
-			for (int i = 0; i < 3; i++) {
-				int nx;
-				if (i < 2) {
-					nx = curr + dx[i];
-				} else {
-					nx = curr * 2;
-				}
-				if ((0 <= nx && nx < INF) && !visited[nx]) {
-					arr[nx] = arr[curr] + 1;
-					queue.add(nx);
-					visited[nx] = true;
-				}
-
-				if (visited[k]) {
-					return arr[k];
-				}
+			int[] curr = queue.poll();
+			int p = curr[0];
+			int time = curr[1];
+			if (p == k) {
+				return time;
 			}
 
+			for (int i = 0; i < 3; i++) {
+				int x = 0;
+				if (i < 2) {
+					x = p + dx[i];
+				} else {
+					x = p * 2;
+				}
+
+				if (x >= INF || x < 0)
+					continue;
+
+				if (!visited[x]) {
+
+					visited[x] = true;
+					queue.offer(new int[] { x, time + 1 });
+				}
+			}
 		}
 		return -1;
 
 	}
+
 }
