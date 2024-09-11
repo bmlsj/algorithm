033d0e27f49b85e7f74d66e9044947a9fb07fba0
bmlsj <97381846+bmlsj@users.noreply.github.com>
2024-09-11 17:09:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 53940 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -8,6 +8,7 @@ public class Main {
 	private static int maxVal;
 
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
 		String[] split = in.readLine().split(" ");
@@ -15,46 +16,29 @@ public class Main {
 		N = Integer.parseInt(split[0]); // 물품 수
 		K = Integer.parseInt(split[1]); // 버틸 수 있는 무게
 
-		int[] weights = new int[N + 1]; // 무게 배열
-		int[] profits = new int[N + 1]; // 가치 배열
-
 		// 1. 동적 테이블 생성
-		int[][] D = new int[N + 1][K + 1];
+		int[][] items = new int[N + 1][K + 1];
 
 		for (int i = 1; i <= N; i++) {
 			split = in.readLine().split(" ");
-			weights[i] = Integer.parseInt(split[0]);
-			profits[i] = Integer.parseInt(split[1]);
-		}
+			int itemWeight = Integer.parseInt(split[0]); // 무게
+			int itemBenefit = Integer.parseInt(split[1]); // 가치
 
-		int itemWeight = 0;
-		int itemBenefit = 0;
+			for (int w = 1; w <= K; w++) {
+				if (itemWeight <= w) {
 
-		for (int item = 1; item <= N; item++) {
-			itemWeight = weights[item]; // 현 아이템의 무게
-			itemBenefit = profits[item]; // 현 아이템의 가치
-
-			// 점화식을 이용하여, 1부터 목표무게까지의 가치 테이블을 만든다.
-			for (int weight = 1; weight <= K; weight++) {
-				// 현 아이템을 선택할 경우(배낭에 담을 수 있는 경우)
-				if (itemWeight <= weight) {
-
-					// 현 아이템을 배낭에 담지 않았을 때 가치(이전 물건까지 담았을 때의 최적해)
-					// 현 아이템을 배낭에 담았을 때의 가치 (새롭게 구한 최적해) 중 유리한 가치를 저장
-					D[item][weight] = Math.max(D[item - 1][weight], itemBenefit + D[item - 1][weight - itemWeight]);
+					items[i][w] = Math.max(items[i - 1][w], itemBenefit + items[i - 1][w - itemWeight]);
 				}
 
-				// 현 아이템을 선택하지 않는 경우(배낭에 담을 수 없는 경우)
 				else {
-					// 이전까지 고려한 물건들의 가치를 그대로 사용
-					D[item][weight] = D[item - 1][weight];
+					items[i][w] = items[i - 1][w];
 
 				}
 			}
 		}
 
 		// 결과 출력
-		System.out.println(D[N][K]);
+		System.out.println(items[N][K]);
 
 	}
 }
