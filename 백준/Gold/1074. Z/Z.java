0733edea7427fe35652da0914c75b5e4c30fd78b
seungji <97381846+bmlsj@users.noreply.github.com>
2024-10-09 22:25:42 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,27 +1,27 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	private static int N, r, c;
-	private static int result;
-	private static int[][] map;
+	static int N, r, c;
+	static long result;
 
 	public static void main(String[] args) throws Exception {
+
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String[] split = in.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		N = (int) Math.pow(2, Integer.parseInt(st.nextToken()));
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
 
-		N = (int) Math.pow(2,Integer.parseInt(split[0]));
-		r = Integer.parseInt(split[1]);
-		c = Integer.parseInt(split[2]);
 		result = 0;
-		start(0, 0, N );
-
+		fill(0, 0, N);
+		
 		System.out.println(result);
 	}
 
-	private static void start(int x, int y, int size) {
+	private static void fill(int x, int y, int size) {
 
 		if (size == 1) {
 			return;
@@ -29,27 +29,24 @@ public class Main {
 
 		int half = size / 2;
 
-		if (r < x + half && c < y + half) { // 좌상
+		if (r < x + half && c < y + half) { // 좌상(시작 점)
 
-			start(x, y, half);
+			fill(x, y, half);
 
 		} else if (r < x + half && c >= y + half) { // 우상
 
 			result += half * half;
-			start(x, y + half, half);
+			fill(x, y + half, half);
 
 		} else if (r >= x + half && c < y + half) { // 좌하
 
 			result += 2 * half * half;
-			start(x + half, y, half);
+			fill(x + half, y, half);
 
-		} else { // 우하
-
+		} else {
 			result += 3 * half * half;
-			start(x + half, y + half, half);
-
+			fill(x + half, y + half, half);
 		}
-
 	}
 
 }
