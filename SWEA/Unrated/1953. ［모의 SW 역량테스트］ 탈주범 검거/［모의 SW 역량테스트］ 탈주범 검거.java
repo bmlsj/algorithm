f1248b8a743ad903b2fd51dfc7e4a60126b5cd4a
seungji <97381846+bmlsj@users.noreply.github.com>
2024-11-12 22:36:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 155 ms, Memory: 27,568 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -1,104 +1,88 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
-import java.util.zip.Inflater;
 
-public class Solution {
+class Solution {
+    static int N, M, R, C, L;
+    static int map[][];
+    static int visited[][];
+    static int ans;
+    static boolean tunnel[][] = { { false, false, false, false }, { true, true, true, true },
+            { true, true, false, false }, { false, false, true, true }, { true, false, false, true },
+            { false, true, false, true }, { false, true, true, false }, { true, false, true, false } };
 
-	private static int T, N, M, R, C, L;
-	private static int[][] map;
-	private static boolean[][] visited;
-	private static int[][] possible = { {}, // 0번 파이프 (존재하지 않음)
-			{ 0, 1, 2, 3 }, // 1번 파이프: 상하좌우
-			{ 0, 1 }, // 2번 파이프: 상하
-			{ 2, 3 }, // 3번 파이프: 좌우
-			{ 0, 3 }, // 4번 파이프: 상우
-			{ 1, 3 }, // 5번 파이프: 하우
-			{ 1, 2 }, // 6번 파이프: 하좌
-			{ 0, 2 } // 7번 파이프: 상좌
-	};
+    static int dx[] = { -1, 1, 0, 0 }; // 상, 하, 좌, 우
+    static int dy[] = { 0, 0, -1, 1 };
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		T = Integer.parseInt(in.readLine());
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int t = 1; t <= T; t++) {
+        StringBuilder sb = new StringBuilder();
 
-			String[] split = in.readLine().split(" ");
-			N = Integer.parseInt(split[0]); // 세로
-			M = Integer.parseInt(split[1]); // 가로
-			R = Integer.parseInt(split[2]); // 맨홀 세로
-			C = Integer.parseInt(split[3]); // 맨홀 가로
-			L = Integer.parseInt(split[4]); // 소요 시간
+        int T = Integer.parseInt(in.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            sb.append("#" + test_case + " ");
 
-			map = new int[N][M];
-			visited = new boolean[N][M];
+            String[] value = in.readLine().split(" ");
 
-			for (int i = 0; i < N; i++) {
-				split = in.readLine().split(" ");
-				for (int j = 0; j < M; j++) {
-					map[i][j] = Integer.parseInt(split[j]);
-				}
-			}
+            N = Integer.parseInt(value[0]);
+            M = Integer.parseInt(value[1]);
+            R = Integer.parseInt(value[2]);
+            C = Integer.parseInt(value[3]);
+            L = Integer.parseInt(value[4]);
 
-			int ans = bfs(R, C);
-			System.out.println("#" + t + " " + ans);
-		}
+            map = new int[N][M];
+            visited = new int[N][M];
 
-	}
+            for (int i = 0; i < N; i++) {
+                String[] line = in.readLine().split(" ");
+                for (int j = 0; j < M; j++) {
+                    map[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            ans = 0;
+            solve(R, C);
+            sb.append(ans + "\n");
+        }
 
-	private static int[] dx = { -1, 1, 0, 0 }; // 상하좌우
-	private static int[] dy = { 0, 0, -1, 1 };
+        System.out.println(sb);
+    }
 
-	private static int bfs(int x, int y) {
+    private static void solve(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] { x, y });
+        visited[x][y] = 1;
+        ans++;
 
-		visited[x][y] = true;
-		Queue<int[]> queue = new ArrayDeque<int[]>();
-		queue.offer(new int[] { x, y, map[x][y], 1 });
-		int count = 1;
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            x = curr[0];
+            y = curr[1];
 
-		while (!queue.isEmpty()) {
+            if (visited[x][y] == L)
+                return;
 
-			int[] curr = queue.poll();
-			x = curr[0];
-			y = curr[1];
-			int pipe = curr[2];
-			int time = curr[3];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-			if (time >= L)
-				continue;
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+                    continue;
+                if (visited[nx][ny] > 0 || map[nx][ny] == 0)
+                    continue;
 
-			for (int dir : possible[pipe]) {
-
-				int nx = x + dx[dir];
-				int ny = y + dy[dir];
-
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny] || map[nx][ny] == 0) {
-					continue;
-				}
-
-				int nextPipe = map[nx][ny];
-				boolean isConnected = false;
-				for (int nextDir : possible[nextPipe]) {
-					if (nextDir == (dir ^ 1)) {
-						isConnected = true;
-						break;
-					}
-				}
-
-				if (isConnected) {
-					visited[nx][ny] = true;
-					queue.offer(new int[] { nx, ny, nextPipe, time + 1 });
-					count++;
-				}
-			}
-		}
-
-		return count;
-
-	}
-
+                if ((i == 0 && tunnel[map[x][y]][0] && tunnel[map[nx][ny]][1])
+                        || (i == 1 && tunnel[map[x][y]][1] && tunnel[map[nx][ny]][0])
+                        || (i == 2 && tunnel[map[x][y]][2] && tunnel[map[nx][ny]][3])
+                        || (i == 3 && tunnel[map[x][y]][3] && tunnel[map[nx][ny]][2])) {
+                    queue.offer(new int[] { nx, ny });
+                    visited[nx][ny] = visited[x][y] + 1;
+                    ans++;
+                }
+            }
+        }
+    }
 }
