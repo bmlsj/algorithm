c16db7e64cb04fca9485a40c227a6726816c0efb
seungji <97381846+bmlsj@users.noreply.github.com>
2025-06-09 23:14:03 +0900

    [Silver IV] Title: 큐, Time: 284 ms, Memory: 20976 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,46 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> queue = new ArrayDeque<>();
 
-		int n = Integer.parseInt(br.readLine());
-		LinkedList<Integer> queue = new LinkedList<>();
-		
-		for(int i = 0; i < n;i++) {
-			String[] split = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            String[] split = br.readLine().split(" ");
 
-			switch (split[0]) {
-				case "pop":
-					System.out.println(queue.isEmpty() ? -1 : queue.poll());
-					break;
-				
-				case "front":
-					System.out.println(queue.isEmpty() ? -1 : queue.peek());
-					break;
-					
-				case "back":
-					System.out.println(queue.isEmpty() ? -1 : queue.getLast());
-					break;
-				
-				
-				case "size":
-					System.out.println(queue.size());
-					break;
-				
-				case "empty":
-					System.out.println(queue.isEmpty() ? 1 : 0);
-					break;
-				default:
-					queue.offer(Integer.parseInt(split[1]));
-					break;
-			}
-		}
+            switch (split[0]) {
+                case "pop":
+                    System.out.println(queue.isEmpty() ? -1 : queue.pollFirst());
+                    break;
 
-	}
+                case "front":
+                    System.out.println(queue.isEmpty() ? -1 : queue.peekFirst());
+                    break;
 
+                case "back":
+                    System.out.println(queue.isEmpty() ? -1 : queue.peekLast());
+                    break;
+
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+
+                case "empty":
+                    System.out.println(queue.isEmpty() ? 1 : 0);
+                    break;
+
+                default: // push X
+                    queue.offerLast(Integer.parseInt(split[1]));
+                    break;
+            }
+        }
+    }
 }
