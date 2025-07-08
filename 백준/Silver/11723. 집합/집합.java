fed6c8ba2931876799e7800eda37bf92141e4bc8
seungji <97381846+bmlsj@users.noreply.github.com>
2025-07-08 19:08:49 +0900

    [Silver V] Title: 집합, Time: 1548 ms, Memory: 300976 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/*
+ * hashmap의 평균 시간 복잡도
+ * 삽입/삭제/탐색 : o(1)
+ */
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int M = Integer.parseInt(br.readLine());
+		HashSet<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < M; i++) {
+			String[] split = br.readLine().split(" ");
+
+			String cmd = split[0];
+			int x = 0;
+			if (split.length > 1) {
+				x = Integer.parseInt(split[1]);
+			}
+
+			switch (cmd) {
+			case "add":
+				set.add(x);
+				break;
+			case "remove":
+				set.remove(x);
+				break;
+			case "check":
+				bw.write(set.contains(x) ? "1\n" : "0\n");
+				break;
+			case "toggle":
+				if (set.contains(x)) {
+					set.remove(x);
+				} else {
+					set.add(x);
+				}
+				break;
+			case "all":
+				set.clear();
+				for (int j = 1; j <= 20; j++) {
+					set.add(j);
+				}
+				break;
+			case "empty":
+				set.clear();
+				break;
+			}
+		}
+		
+		bw.flush();  
+        bw.close();
+        br.close();
+
+	}
+
+}
