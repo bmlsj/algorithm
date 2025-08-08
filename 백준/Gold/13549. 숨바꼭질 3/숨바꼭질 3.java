a6af92b29e1601e9d01d63d1efc964820916c130
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2025-08-08 15:35:32 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 24612 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static boolean[] visited;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        N = Integer.parseInt(split[0]);
+        K = Integer.parseInt(split[1]);
+        
+        visited = new boolean[200001];
+        System.out.println(bfs());
+    }
+    
+    static int bfs() {
+    	
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[]{N, 0});
+        visited[N] = true;
+        
+        while (!dq.isEmpty()) {
+        	
+            int[] cur = dq.pollFirst();
+            int x = cur[0];
+            int time = cur[1];
+            
+            if (x == K) return time;
+            
+            // 순간이동 (가중치 0)
+            int nx = x * 2;
+            if (nx <= 200000 && !visited[nx]) {
+                visited[nx] = true;
+                dq.addFirst(new int[]{nx, time});
+            }
+            
+            // -1, +1 이동 (가중치 1)
+            for (int dir : new int[]{-1, 1}) {
+                nx = x + dir;
+                if (nx >= 0 && nx <= 200000 && !visited[nx]) {
+                    visited[nx] = true;
+                    dq.addLast(new int[]{nx, time + 1});
+                }
+            }
+        }
+        return -1;
+    }
+}
