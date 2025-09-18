fd8683654e3dd040e65a49187d11a34ee176bafb
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2025-09-18 17:50:43 +0900

    [level 2] Title: 방문 길이, Time: 2.09 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -8,19 +8,6 @@ class Solution {
 			this.x = x;
 			this.y = y;
 		}
-        
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Point)) return false;
-            Point p = (Point) o;
-            return x == p.x && y == p.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
 	}
     
     static class Line {
