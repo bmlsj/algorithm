f20b82f7180255c15066b77d826e76a19515f41e
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2025-10-31 21:57:34 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 0.21 ms
+메모리: 73 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 16일 10:01:56
+2025년 10월 31일 21:57:32
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,7 +1,50 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> deploy = new ArrayList<>();
+        
+        for(int i = 0; i < speeds.length; i++) {
+            int day = (100 - progresses[i]) / speeds[i];
+            if((100 - progresses[i]) % speeds[i] != 0) {
+                day += 1;
+            } 
+            deploy.add(day);
+        }
+        
         List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < deploy.size(); i++) {
+            int num = deploy.get(i);
+            int cnt = 1;
+            boolean found = false;
+            for(int j = i + 1; j < deploy.size(); j++) {
+                int next = deploy.get(j);
+                if (num >= next) {
+                    cnt++;
+                } else {
+                    i = j - 1;
+                    ans.add(cnt);
+                    found = true;
+                    break;
+                }
+              
+            }
+            
+            if (!found) {
+                ans.add(cnt);
+                break;
+            }
+        }
+
+        int[] answer = new int[ans.size()];
+        for(int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
+        return answer;
+    }
+}
+
+/*
+  List<Integer> ans = new ArrayList<>();
 		int idx  = 0;
 		
 		while (true) {
@@ -29,5 +72,4 @@ class Solution {
 			res[i] = ans.get(i);
 		}
 		return res;
-    }
-}
+        */
