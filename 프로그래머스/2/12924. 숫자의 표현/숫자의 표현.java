e2e29a287984591489fce03785859214ab4a09b5
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2025-11-21 16:16:38 +0900

    [level 2] Title: 숫자의 표현, Time: 1.67 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+   
+        for(int i = 1; i <= n; i++) {
+            int num = i;
+            for(int j = i + 1; j <= n; j++) {
+                num += j;
+                if (num == n) {
+                    answer++;
+                    break;
+                }
+                if (num > n) {
+                    break;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
