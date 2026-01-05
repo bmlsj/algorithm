57d280ffabed8e22ed5f2c6271476c0a4584dab2
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2026-01-05 21:48:27 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 112 ms, Memory: 14684 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int t = 1;  t <= n; t++) {
+			System.out.printf("Material Management %d%n", t);
+			System.out.println("Classification ---- End!");
+		}
+		
+	}
+	
+
+}
