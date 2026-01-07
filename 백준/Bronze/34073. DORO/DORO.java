61735154d225f1848d00f5ace30073c8e0c2d39a
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2026-01-07 18:04:34 +0900

    [Bronze IV] Title: DORO, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] split = br.readLine().split(" ");
+		
+		StringBuilder sb = new StringBuilder();
+		for(String s: split) {
+			sb.append(s + "DORO ");
+		}
+		
+		System.out.println(sb.toString());
+	}
+
+}
