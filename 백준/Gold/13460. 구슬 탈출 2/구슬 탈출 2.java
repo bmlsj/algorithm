802beb70f61354f2f64cbf2d44622720027a9443
SEUNGJI <97381846+bmlsj@users.noreply.github.com>
2026-04-06 17:17:58 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 116 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -4,121 +4,135 @@ import java.util.*;
 
 public class Main {
 
+	/*
+	 * 빨간 구슬이 구멍에 빠져야함. 파란구슬이 먼저 빠지면 안됨 방향 전환한 최소 횟수를 구하는 것
+	 */
 	static int N, M;
 	static char[][] map;
-	static boolean[][][][] visited;
-	static int cnt;
-
-	static class State {
-		int rx, ry, bx, by, depth;
-
-		State(int rx, int ry, int bx, int by, int depth) {
-			this.rx = rx;
-			this.ry = ry;
-			this.bx = bx;
-			this.by = by;
-			this.depth = depth;
-		}
-	}
+	static int rx, ry, bx, by, ex, ey;
 
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] split = br.readLine().split(" ");
-		N = Integer.parseInt(split[0]);
-		M = Integer.parseInt(split[1]);
+		String[] input;
 
+		// N, M
+		input = br.readLine().split(" ");
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+
+		// 지도
 		map = new char[N][M];
-		int rx = -1, ry = -1;
-		int bx = -1, by = -1;
 		for (int i = 0; i < N; i++) {
-			split = br.readLine().split("");
+			input = br.readLine().split("");
 			for (int j = 0; j < M; j++) {
-				map[i][j] = split[j].charAt(0);
+				map[i][j] = input[j].charAt(0);
 				if (map[i][j] == 'R') {
 					rx = i;
 					ry = j;
 				} else if (map[i][j] == 'B') {
 					bx = i;
 					by = j;
+				} else if (map[i][j] == 'O') {
+					ex = i;
+					ey = j;
 				}
 			}
 		}
 
-		visited = new boolean[N][M][N][M];
-		cnt = 0;
-		bfs(rx, ry, bx, by);
+		int ans = bfs();
+		System.out.println(ans);
+
 	}
 
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
 
-	static int[] move(int x, int y, int dir) {
-		int cnt = 0;
-		while (true) {
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
+	static int bfs() {
 
-			if (map[nx][ny] == '#')
-				break;
-			x = nx;
-			y = ny;
-			cnt++;
-			if (map[x][y] == 'O')
-				break;
-		}
+		Queue<int[]> q = new ArrayDeque<>();
+		boolean[][][][] visited = new boolean[N][M][N][M];
 
-		return new int[] { x, y, cnt };
-	}
+		q.offer(new int[] { rx, ry, bx, by, 1 });
+		visited[rx][ry][bx][by] = true;
 
-	static void bfs(int x, int y, int bx, int by) {
+		while (!q.isEmpty()) {
 
-		visited[x][y][bx][by] = true;
-		Queue<State> queue = new ArrayDeque<State>();
-		queue.offer(new State(x, y, bx, by, 0));
+			int[] cur = q.poll();
+			rx = cur[0];
+			ry = cur[1];
+			bx = cur[2];
+			by = cur[3];
+			int cnt = cur[4];
 
-		while (!queue.isEmpty()) {
-
-			State curr = queue.poll();
-
-			if (curr.depth >= 10) {
-				System.out.println("-1");
-				return;
+			if (cnt > 10) {
+				return -1;
 			}
 
-			for (int i = 0; i < 4; i++) {
-				int[] r = move(curr.rx, curr.ry, i);
-				int[] b = move(curr.bx, curr.by, i);
+			for (int d = 0; d < 4; d++) {
+				int rnx = rx;
+				int rny = ry;
+				int bnx = bx;
+				int bny = by;
 
-				// 파란공이 빠질 경우
-				if (map[b[0]][b[1]] == 'O') {
-                    continue;
-                }
-				// 빨간 공이 빠질 경우
-				if (map[r[0]][r[1]] == 'O') {
-					System.out.println(curr.depth + 1);
-					return;
-				}
+				boolean isRed = false;
+				boolean isBlue = false;
+				int rDist = 0, bDist = 0;
 
-				if (r[0] == b[0] && r[1] == b[1]) {
-					if (r[2] > b[2]) { // 빨간공이 왼쪽에 잇으므로 | 빨 | 파 | 이렇게 위치
-						r[0] -= dx[i];
-						r[1] -= dy[i];
-					} else {
-						b[0] -= dx[i];
-						b[1] -= dy[i];
+				// red
+				while (map[rnx + dx[d]][rny + dy[d]] != '#') {
+
+					rnx += dx[d];
+					rny += dy[d];
+					rDist++;
+
+					if (map[rnx][rny] == 'O') {// 이동 후가 구멍일 경우
+						isRed = true;
+						break;
 					}
 				}
 
-				if (!visited[r[0]][r[1]][b[0]][b[1]]) {
-					visited[r[0]][r[1]][b[0]][b[1]] = true;
-					queue.offer(new State(r[0], r[1], b[0], b[1], curr.depth + 1));
+				// blue
+				while (map[bnx + dx[d]][bny + dy[d]] != '#') {
+
+					bnx += dx[d];
+					bny += dy[d];
+					bDist++;
+
+					if (map[bnx][bny] == 'O') {// 이동 후가 구멍일 경우
+						isBlue = true;
+						break;
+					}
+				}
+
+				if (isBlue) { // 파란 구슬이 먼저 도착
+					continue;
+				}
+
+				if (isRed && !isBlue) {
+					return cnt;
+				}
+
+				if (rnx == bnx && rny == bny) {
+					if (rDist > bDist) { // 빨간 구슬이 더 멀리 있을 때(파란 구슬 전칸으로 넘겨야함)
+						rnx -= dx[d];
+						rny -= dy[d];
+					} else {
+						bnx -= dx[d];
+						bny -= dy[d];
+					}
+				}
+
+				if (!visited[rnx][rny][bnx][bny]) {
+					visited[rnx][rny][bnx][bny] = true;
+					q.offer(new int[] { rnx, rny, bnx, bny, cnt + 1 });
 				}
 
 			}
-
 		}
-        System.out.println("-1");
+
+		return -1;
 
 	}
+
 }
